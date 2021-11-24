c56d80bd93ce3a2224ad067443111fda1fcfa63e
restuhanputra <restu.hanputraa@gmail.com>
2021-11-25 02:55:22 +0700

    add: Kategori create view

---
+++ application/views/kategori/create.php
@@ -0,0 +1,56 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0"><?= $title ?></h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="<?= base_url('dashboard') ?>">Dashboard</a></li>
+            <li class="breadcrumb-item active"><?= $title ?></li>
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+
+        <!-- col-12 -->
+        <div class="col-6">
+          <div class="card">
+            <!-- /.card-header -->
+            <div class="card-body">
+              <form action="" method="POST">
+                <div class="form-group">
+                  <label for="kategori">Nama Kategori</label>
+                  <input type="text" name="kategori" class="form-control <?= form_error('kategori') ? 'is-invalid' : ''; ?>" placeholder="Nama Kategori" value="<?= set_value('kategori') ?>">
+                  <span class="error invalid-feedback"><?= form_error('kategori') ?></span>
+                </div>
+                <div class="text-center">
+                  <a href="<?= base_url('kategori') ?>" class="btn btn-warning">Kembali</a>
+                  <button type="submit" class="btn btn-primary">Simpan</button>
+                </div>
+              </form>
+            </div>
+            <!-- /.card-body -->
+          </div>
+          <!-- /.card -->
+        </div>
+        <!-- /.col-12 -->
+
+      </div>
+      <!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
