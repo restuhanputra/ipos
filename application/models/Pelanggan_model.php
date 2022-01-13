908765466560f82888c1ecf89fa13434a778ff35
restuhanputra <restu.hanputraa@gmail.com>
2022-01-13 14:43:05 +0700

    add: Pelanggan_model

---
+++ application/models/Pelanggan_model.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Pelanggan_model extends CI_Model
+{
+  public function __construct()
+  {
+    parent::__construct();
+    $this->table = 'pelanggan';
+  }
+
+  public function getAllData()
+  {
+    return $this->db->get($this->table);
+  }
+
+  public function insert($data)
+  {
+    $this->db->insert($this->table, $data);
+    return $this->db->affected_rows();
+  }
+
+  public function getDataBy($data)
+  {
+    return $this->db->get_where($this->table, $data);
+  }
+
+  public function delete($data)
+  {
+    $this->db->delete($this->table, $data);
+    return $this->db->affected_rows();
+  }
+
+  public function update($data, $where)
+  {
+    $this->db->update($this->table, $data, $where);
+    return $this->db->affected_rows();
+  }
+}
