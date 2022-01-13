bba05c9316e6b515a792be6828f028dfa83b8fc3
restuhanputra <restu.hanputraa@gmail.com>
2022-01-13 18:32:23 +0700

    update: comment laporan

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -65,9 +65,9 @@
           </div>
         </div>
         <!-- ./col -->
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-warning">
+        <!-- <div class="col-lg-3 col-6"> -->
+        <!-- small box -->
+        <!-- <div class="small-box bg-warning">
             <div class="inner">
               <h3>65</h3>
 
@@ -75,11 +75,10 @@
             </div>
             <div class="icon">
               <i class="fas fa-file-invoice-dollar"></i>
-              <!-- <i class="ion ion-pie-graph"></i> -->
             </div>
             <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
           </div>
-        </div>
+        </div> -->
         <!-- ./col -->
         <div class="col-lg-3 col-6">
           <!-- small box -->
